e96ca911667cdb611f49bd3ee4d7dfeba071f920
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-08-30 09:57:49 +0200

    Add search by use chrome.search.query

--- background.js
+++ background.js
@@ -7,20 +7,10 @@ chrome.runtime.onInstalled.addListener((details) => {
 
   chrome.contextMenus.onClicked.addListener((event) => {
     console.log(event);
-  });
-
-  chrome.contextMenus.create({
-    title: "Text Context Menu 1",
-    id: "contextMenu2",
-    parentId: "contextMenu1",
-    contexts: ["page", "selection"],
-  });
-
-  chrome.contextMenus.create({
-    title: "Text Context Menu 2",
-    id: "contextMenu3",
-    parentId: "contextMenu1",
-    contexts: ["page", "selection"],
+    chrome.search.query({
+      disposition: "NEW_TAB",
+      text: `imdb ${event.selectionText}`,
+    });
   });
 });
 
