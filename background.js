3b51743968f3af386d41f4b0453f5f147cb3f69f
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-08-30 10:16:24 +0200

    Add search by use chrome.tabs.create

--- background.js
+++ background.js
@@ -7,9 +7,8 @@ chrome.runtime.onInstalled.addListener((details) => {
 
   chrome.contextMenus.onClicked.addListener((event) => {
     console.log(event);
-    chrome.search.query({
-      disposition: "NEW_TAB",
-      text: `imdb ${event.selectionText}`,
+    chrome.tabs.create({
+      url: `https://www.imdb.com/find/?q=${event.selectionText}&ref_=nv_sr_sm`,
     });
   });
 });
